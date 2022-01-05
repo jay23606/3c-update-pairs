7743679bcca094479f3d6bb74168d3b9e9ae182a
Jay <jay_23606@yahoo.com>
2022-01-05 12:59:20 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,28 +11,32 @@ namespace update_pairs
 {
     class Program
     {
-        //update with your 3c api key/secret and exchange info
+        //update with your info
         static string key = "xxx";
         static string secret = "xxx";
         static string market = "kucoin";
+        static string baseType = "USDT";
+        static bool usePerp = false;
+        static int botId = 7434870;
         static int accountId = 0;
-        static int botId = 1234567;
+        
         public static XCommasApi api;
         public static HttpClient hc = new HttpClient();
 
-        public static int MAX_ALTRANK_PAIRS = 50;
-        public static int MAX_GALAXY_PAIRS = 50;
+        public static int MAX_ALTRANK_PAIRS = 40;
+        public static int MAX_GALAXY_PAIRS = 20;
+
+        static Random r = new Random();
 
         static void Main() { MainAsync().GetAwaiter().GetResult(); }
-        
         static async Task MainAsync()
         {
             api = new XCommasApi(key, secret, default, UserMode.Real);
             var accts = await api.GetAccountsAsync();
-            foreach (var acct in accts.Data) if (acct.MarketCode == market) { accountId = acct.Id; break; }
+            if (accountId != 0) foreach (var acct in accts.Data) if (acct.MarketCode == market) { accountId = acct.Id; break; }
             var marketPairs = await api.GetMarketPairsAsync(market);
             HashSet<string> pairs = new HashSet<string>();
-            foreach (string p in marketPairs.Data) pairs.Add(p); //get available pairs into hashset
+            foreach (string p in marketPairs.Data) if (p.StartsWith(baseType)) pairs.Add(p); //get available USDT pairs 
 
             while (true)
             {
@@ -40,10 +44,11 @@ namespace update_pairs
                 {
                     LunarCrushRoot res = await GetJSON<LunarCrushRoot>("https://api.lunarcrush.com/v2?data=market&type=fast&sort=acr&limit=1000&key=asdf");
                     HashSet<string> pairsToUpdate = new HashSet<string>();
+
                     foreach(Datum d in res.data)
                     {
                         if (pairsToUpdate.Count >= MAX_ALTRANK_PAIRS) break;
-                        string pair = $"USDT_{d.s}";
+                        string pair = $"{baseType}_{d.s}{((usePerp) ? "-PERP" : "")}";
                         if (pairs.Contains(pair) && !pairsToUpdate.Contains(pair)) pairsToUpdate.Add(pair);
                     }
                     int altRankPairsCount = pairsToUpdate.Count;
@@ -51,14 +56,22 @@ namespace update_pairs
                     foreach (Datum d in res.data)
                     {
                         if (pairsToUpdate.Count >= altRankPairsCount + MAX_GALAXY_PAIRS) break;
-                        string pair = $"USDT_{d.s}";
+                        string pair = $"{baseType}_{d.s}{((usePerp) ? "-PERP" : "")}";
                         if (pairs.Contains(pair) && !pairsToUpdate.Contains(pair)) pairsToUpdate.Add(pair);
                     }
-                    
+
+                    //var bots = await api.GetBotsAsync(limit: 1000, accountId: accountId, botId: botId);
                     var sb = await api.ShowBotAsync(botId: botId);
                     Bot bot = sb.Data;
+                    //foreach (Bot b in bots.Data) if (b.Id == botId) { bot = b; break; }
 
                     bot.Pairs = pairsToUpdate.ToArray();
+
+                    
+                    TradingViewBotStrategy tv = (TradingViewBotStrategy)bot.Strategies[0];
+                    if (r.Next(1, 4) == 1) tv.Options.Type = TradingViewIndicatorType.Sell; //occasionally allow sell instead of strong sell
+                    else tv.Options.Type = TradingViewIndicatorType.StrongSell;
+
 
                     var ub = await api.UpdateBotAsync(botId, new BotUpdateData(bot));
                     if (ub.IsSuccess) Console.WriteLine($"Successfully updated {bot.Name} with {pairsToUpdate.Count} new pairs..");
